ab61da9cca3b69f7d646f19816b8216f6039b62d
Laurie Shamseldin <laurie.musson@gmail.com>
2023-02-16 15:11:36 +0100

    Vars scss colors and font family

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,14 +3,12 @@ import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
-function App() {
-  return (
-    <div className="app">
-      <img src={reactLogo} alt="react logo" />
-      <h1>Composant : App</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="app">
+    <img src={reactLogo} alt="react logo" />
+    <h1>Composant : App</h1>
+  </div>
+);
 
 // == Export
 export default App;
