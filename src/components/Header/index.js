cb7c847f0757ea76e0810a2b512544a3806fcbfe
Laurie Shamseldin <laurie.musson@gmail.com>
2023-02-17 09:21:14 +0100

    Add div Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,9 +2,9 @@ import Navbar from '../Navbar';
 import './style.scss';
 
 const Header = () => (
-  <div className="header">
+  <header className="header">
     <Navbar classColor="logo white" />
-  </div>
+  </header>
 
 );
 
