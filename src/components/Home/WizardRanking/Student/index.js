0944fca1d1c265bbfff34e9bfeabca1c9703a567
MathieuMoye <mathieu.afev66@gmail.com>
2023-02-17 17:23:54 +0100

    intégration home

--- src/components/Home/WizardRanking/Student/index.js
+++ src/components/Home/WizardRanking/Student/index.js
@@ -1,17 +1,30 @@
-const Student = () => (
-  <ul>
-    <li>
-      <p>
-        Mathieu
-      </p>
-      <p>
-        Gryffondor
-      </p>
-      <p>
-        175 points
+import PropTypes from 'prop-types';
+import './style.scss';
+import houseLogo from '../../../../assets/img/blason-gryffondor.png';
+
+const Student = ({ name, house, points }) => (
+  <ul className="student-list">
+    <li className="student-item">
+      <img className="student-img" src={houseLogo} alt="blason" />
+      <div className="text-container">
+        <p className="student-name">
+          {name}
+        </p>
+        <p className="student-house">
+          {house}
+        </p>
+      </div>
+      <p className="student-point">
+        {points}points
       </p>
     </li>
   </ul>
 );
 
 export default Student;
+
+Student.propTypes = {
+  name: PropTypes.string.isRequired,
+  house: PropTypes.string.isRequired,
+  points: PropTypes.number.isRequired,
+};
--- src/components/Home/WizardRanking/index.js
+++ src/components/Home/WizardRanking/index.js
@@ -1,13 +1,14 @@
 import Student from './Student';
+import './style.scss';
 
 const WizardRanking = () => (
   <div>
     <h1>Classement des meilleurs sorciers</h1>
-    <Student />
-    <Student />
-    <Student />
-    <Student />
-    <Student />
+    <Student name="Mathieu" house="Gryffondor" points={175} />
+    <Student name="David" house="Serpentard" points={160} />
+    <Student name="Jean-Matthieu" house="Serdaigle" points={145} />
+    <Student name="Gigi" house="Poufsouffle" points={130} />
+    <Student name="Laurie" house="Serpentard" points={110} />
   </div>
 );
 
