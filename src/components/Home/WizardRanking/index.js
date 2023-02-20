42c1ab3ecbf96e8ace1847e5db297dce30d0fd5c
MathieuMoye <mathieu.afev66@gmail.com>
2023-02-20 15:51:31 +0100

    dynamisation classement sorcier

--- src/components/Home/WizardRanking/index.js
+++ src/components/Home/WizardRanking/index.js
@@ -4,11 +4,11 @@ import './style.scss';
 const WizardRanking = () => (
   <div>
     <h1>Classement des meilleurs sorciers</h1>
-    <Student name="Mathieu" house="Gryffondor" points={175} />
-    <Student name="David" house="Serpentard" points={160} />
-    <Student name="Jean-Matthieu" house="Serdaigle" points={145} />
-    <Student name="Gigi" house="Poufsouffle" points={130} />
-    <Student name="Laurie" house="Serpentard" points={110} />
+    <Student name="Mathieu" house="Gryffondor" points={175} container="text-container-gryffindor" houseName="student-name-gryffindor" houseColor="student-house-gryffindor" />
+    <Student name="David" house="Serpentard" points={160} container="text-container-slytherin" houseName="student-name-slytherin" houseColor="student-house-slytherin" />
+    <Student name="Jean-Matthieu" house="Serdaigle" points={145} container="text-container-ravenclaw" houseName="student-name-ravenclaw" houseColor="student-house-ravenclaw" />
+    <Student name="Gigi" house="Poufsouffle" points={130} container="text-container-hufflepuff" houseName="student-name-hufflepuff" houseColor="student-house-hufflepuff" />
+    <Student name="Laurie" house="Serpentard" points={110} container="text-container-slytherin" houseName="student-name-slytherin" houseColor="student-house-slytherin" />
   </div>
 );
 
