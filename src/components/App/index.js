2aa94979e73c8223481cef0dfb87547e6642b4a0
Laurie Shamseldin <laurie.musson@gmail.com>
2023-02-20 15:47:57 +0100

    Add account route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from '../Footer';
 import Home from '../Home';
 import Login from '../Login';
+import Account from '../Account';
 import './styles.css';
 
 // == Composant
@@ -12,6 +13,7 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/account" element={<Account />} />
     </Routes>
     <Footer />
   </div>
