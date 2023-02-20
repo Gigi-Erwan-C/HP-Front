a37eea4978508044d9b3f2275fb1aefa3c771f36
David-Ress <david.ressicaud@hotmail.fr>
2023-02-20 15:59:20 +0100

    ajout du composant Lore qui contiendra les histoires des maisons

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from '../Footer';
 import Home from '../Home';
 import Login from '../Login';
+import Lore from '../Lore';
 import './styles.css';
 
 // == Composant
@@ -12,6 +13,7 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/histoire" element={<Lore />} />
     </Routes>
     <Footer />
   </div>
---
+++ src/components/Lore/index.js
@@ -0,0 +1,13 @@
+import Header from '../Header';
+
+const Lore = () => (
+  <div>
+    <Header />
+    <div>
+      
+    </div>
+  </div>
+
+);
+
+export default Lore;
