5a21fea104f04cb93ed488ded9cf8afe812e1af7
Laurie Shamseldin <laurie.musson@gmail.com>
2023-02-20 11:49:22 +0100

    Add composant Login

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,12 @@
 // == Import
 import Footer from '../Footer';
 import Home from '../Home';
-import Login from '../Login';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-    {/* <Home /> */}
-    <Login />
+    <Home />
     <Footer />
   </div>
 
