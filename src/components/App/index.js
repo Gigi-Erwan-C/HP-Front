dcec8f277e3f3b99d47f67ba7f99872d59c6c5bb
Laurie Shamseldin <laurie.musson@gmail.com>
2023-02-21 14:38:00 +0100

    Change composant App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,7 @@ import Footer from '../Footer';
 import Home from '../Home';
 import Login from '../Login';
 import Account from '../Account';
+import Rules from '../Rules';
 import './styles.css';
 
 // == Composant
@@ -12,6 +13,7 @@ const App = () => (
   <div className="app">
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/regles" element={<Rules />} />
       <Route path="/login" element={<Login />} />
       <Route path="/account" element={<Account />} />
     </Routes>
