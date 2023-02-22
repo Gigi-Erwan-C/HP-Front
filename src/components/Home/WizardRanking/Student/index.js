10ba471e4ec1ec4357501d2fb232b5318a0624fd
David-Ress <david.ressicaud@hotmail.fr>
2023-02-22 10:59:27 +0100

    dynamisation wizard ranking avec de fausses données + correction du css

--- src/components/Home/WizardRanking/Student/index.js
+++ src/components/Home/WizardRanking/Student/index.js
@@ -1,26 +1,27 @@
+/* eslint-disable camelcase */
 import PropTypes from 'prop-types';
 import './style.scss';
 import houseLogo from '../../../../assets/img/blason-gryffondor.png';
 
 const Student = ({
-  name, house,
-  points, container,
-  houseName,
-  houseColor,
+  firstname,
+  house_name,
+  score,
+  house_name_in_english,
 }) => (
   <ul className="student-list">
     <li className="student-item">
       <img className="student-img" src={houseLogo} alt="blason" />
-      <div className={container}>
-        <p className={houseName}>
-          {name}
+      <div className={`text-container ${house_name_in_english.toLowerCase()}-border`}>
+        <p className={`student-name ${house_name_in_english.toLowerCase()}`}>
+          {firstname}
         </p>
-        <p className={houseColor}>
-          {house}
+        <p className={`student-house ${house_name_in_english.toLowerCase()}`}>
+          {house_name}
         </p>
       </div>
       <p className="student-point">
-        {points} points
+        {score} points
       </p>
     </li>
   </ul>
@@ -29,10 +30,8 @@ const Student = ({
 export default Student;
 
 Student.propTypes = {
-  name: PropTypes.string.isRequired,
-  house: PropTypes.string.isRequired,
-  points: PropTypes.number.isRequired,
-  container: PropTypes.string.isRequired,
-  houseName: PropTypes.string.isRequired,
-  houseColor: PropTypes.string.isRequired,
+  firstname: PropTypes.string.isRequired,
+  house_name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  house_name_in_english: PropTypes.string.isRequired,
 };
--- src/components/Home/WizardRanking/index.js
+++ src/components/Home/WizardRanking/index.js
@@ -1,14 +1,15 @@
 import Student from './Student';
+import studentData from '../../../db/students.json';
 import './style.scss';
 
 const WizardRanking = () => (
   <div className="wizard-ranking">
     <h2 className="wizard-type">Classement des meilleurs sorciers</h2>
-    <Student name="Mathieu" house="Gryffondor" points={175} container="text-container-gryffindor" houseName="student-name-gryffindor" houseColor="student-house-gryffindor" />
-    <Student name="David" house="Serpentard" points={160} container="text-container-slytherin" houseName="student-name-slytherin" houseColor="student-house-slytherin" />
-    <Student name="Jean-Matthieu" house="Serdaigle" points={145} container="text-container-ravenclaw" houseName="student-name-ravenclaw" houseColor="student-house-ravenclaw" />
-    <Student name="Gigi" house="Poufsouffle" points={130} container="text-container-hufflepuff" houseName="student-name-hufflepuff" houseColor="student-house-hufflepuff" />
-    <Student name="Laurie" house="Serpentard" points={110} container="text-container-slytherin" houseName="student-name-slytherin" houseColor="student-house-slytherin" />
+    {studentData.map((student) => (
+      <Student
+        {...student}
+      />
+    ))}
   </div>
 );
 
