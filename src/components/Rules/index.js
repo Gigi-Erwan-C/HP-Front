f7693be60b5827ad47b5b4e1b6f7112de88fe4d9
Laurie Shamseldin <laurie.musson@gmail.com>
2023-02-22 13:30:49 +0100

    Expand for rules page

--- src/components/Rules/index.js
+++ src/components/Rules/index.js
@@ -12,212 +12,186 @@ const Rules = () => (
     <Header />
     <div className="rules">
       <h1>Les droits des sorciers</h1>
-
       <div className="rules-item">
-        <div className="rules-item-title-left">
-          <h2>1. Fonctionnement général</h2>
-        </div>
-        <div className="rules-item-content">
-          <img className="rules-img-left" src={rule1} alt="Regle n°1" />
-          <div className="rules-item-content-text">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+        <section className="accordion">
+          <input type="checkbox" name="collapse" id="handle1" />
+          <h2 className="handle">
+            <label for="handle1">1. Fonctionnement général</label>
+          </h2>
+          <div className="content rules-item-content">
+            <img className="rules-img-left" src={rule1} alt="Regle n°1" />
+            <div className="rules-item-content-text">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
+            </div>
           </div>
-        </div>
-      </div>
+        </section>
 
-      <div className="rules-item">
-        <div className="rules-item-title-right">
-          <h2>2. Gagner et perdre des points</h2>
-        </div>
-        <div className="rules-item-content">
-          <div className="rules-item-content-text">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+        <section className="accordion">
+          <input type="checkbox" name="collapse" id="handle2" />
+          <h2 className="handle">
+            <label for="handle2">2. Gagner et perdre des points</label>
+          </h2>
+          <div className="content rules-item-content">
+            <div className="rules-item-content-text">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
+            </div>
+            <img className="rules-img-right" src={rule2} alt="Regle n°2" />
           </div>
-          <img className="rules-img-right" src={rule2} alt="Regle n°2" />
-        </div>
-      </div>
+        </section>
 
-      <div className="rules-item">
-        <div className="rules-item-title-left">
-          <h2>3. Répartition des sorciers</h2>
-        </div>
-        <div className="rules-item-content">
-          <img className="rules-img-left" src={rule3} alt="Regle n°3" />
-          <div className="rules-item-content-text">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+        <section className="accordion">
+          <input type="checkbox" name="collapse" id="handle3" />
+          <h2 className="handle">
+            <label for="handle3">3. Répartition des sorciers</label>
+          </h2>
+          <div className="content rules-item-content">
+            <img className="rules-img-left" src={rule3} alt="Regle n°3" />
+            <div className="rules-item-content-text">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
+            </div>
           </div>
-        </div>
-      </div>
+        </section>
 
-      <div className="rules-item">
-        <div className="rules-item-title-right">
-          <h2>4. Les duels de Sorciers</h2>
-        </div>
-        <div className="rules-item-content">
-          <div className="rules-item-content-text">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+        <section className="accordion">
+          <input type="checkbox" name="collapse" id="handle4" />
+          <h2 className="handle">
+            <label for="handle4">4. Les duels de Sorciers</label>
+          </h2>
+          <div className="content rules-item-content">
+            <div className="rules-item-content-text">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
+            </div>
+            <img className="rules-img-right" src={rule4} alt="Regle n°4" />
           </div>
-          <img className="rules-img-right" src={rule4} alt="Regle n°4" />
-        </div>
-      </div>
+        </section>
 
-      <div className="rules-item">
-        <div className="rules-item-title-left">
-          <h2>5. Les paliers de classe</h2>
-        </div>
-        <div className="rules-item-content">
-          <img className="rules-img-left" src={rule5} alt="Regle n°5" />
-          <div className="rules-item-content-text">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+        <section className="accordion">
+          <input type="checkbox" name="collapse" id="handle5" />
+          <h2 className="handle">
+            <label for="handle5">5. Les paliers de classe</label>
+          </h2>
+          <div className="content rules-item-content">
+            <img className="rules-img-left" src={rule5} alt="Regle n°5" />
+            <div className="rules-item-content-text">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
+            </div>
           </div>
-        </div>
-      </div>
+        </section>
 
-      <div className="rules-item">
-        <div className="rules-item-title-right">
-          <h2>6. Mon aventure</h2>
-        </div>
-        <div className="rules-item-content">
-          <div className="rules-item-content-text">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+        <section className="accordion">
+          <input type="checkbox" name="collapse" id="handle6" />
+          <h2 className="handle">
+            <label for="handle6">6. Mon aventure</label>
+          </h2>
+          <div className="content rules-item-content">
+            <div className="rules-item-content-text">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
-              urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
-              Praesent dignissim magna a mauris pharetra varius
-            </p>
-
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas, ipsum quis condimentum blandit,
+                urna quam iaculis dolor, nec laoreet lacus ex nec augue. Maecenas placerat purus varius augue lobortis elementum.
+                Praesent dignissim magna a mauris pharetra varius
+              </p>
+            </div>
+            <img className="rules-img-right" src={rule6} alt="Regle n°6" />
           </div>
-          <img className="rules-img-right" src={rule6} alt="Regle n°6" />
-        </div>
+        </section>
+
       </div>
     </div>
   </div>
+
 );
 
 export default Rules;
