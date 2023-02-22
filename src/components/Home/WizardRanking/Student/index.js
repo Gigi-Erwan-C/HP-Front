80a7fe2a8e046fe7ab67ec7ce2fe104775c07f46
David-Ress <david.ressicaud@hotmail.fr>
2023-02-22 11:27:25 +0100

    dynamisé les images, le bon blason s'affiche pour les élèves en top 5

--- src/components/Home/WizardRanking/Student/index.js
+++ src/components/Home/WizardRanking/Student/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable camelcase */
 import PropTypes from 'prop-types';
 import './style.scss';
-import houseLogo from '../../../../assets/img/blason-gryffondor.png';
 
 const Student = ({
   firstname,
   house_name,
   score,
   house_name_in_english,
+  houseLogo
 }) => (
   <ul className="student-list">
     <li className="student-item">
@@ -34,4 +34,5 @@ Student.propTypes = {
   house_name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
   house_name_in_english: PropTypes.string.isRequired,
+  houseLogo: PropTypes.string.isRequired,
 };
